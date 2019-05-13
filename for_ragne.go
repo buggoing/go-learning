a5997e7ca582acbc615bdafd2365720db3a4d57d
yangyou <onbug@qq.com>
2019-05-14 00:44:53 +0800

    update

--- for_ragne.go
+++ for_ragne.go
@@ -11,24 +11,38 @@ func main() {
 		"bar": 2222,
 	}
 	newScore := make(map[string]*int)
+	newScore1 := make(map[string]*int)
 	for key, sc := range score {
 		sco := sc
 		fmt.Println(key, &sc)
 		newScore[key] = &sco
+		newScore1[key] = &sc
 	}
 
 	for key, sc := range newScore {
 		fmt.Println(key, *sc)
 	}
+	for key, sc := range newScore1 {
+		fmt.Println(key, *sc)
+	}
 
 	var wait sync.WaitGroup
-	// runtime.GOMAXPROCS(1)
-	for i := 0; i < 100000; i++ {
+
+	for i := 0; i < 100; i++ {
 		wait.Add(1)
 		go func() {
 			fmt.Println(i)
 			wait.Done()
 		}()
+
+	}
+
+	for i := 200; i < 300; i++ {
+		wait.Add(1)
+		go func(index int) {
+			fmt.Println(index)
+			wait.Done()
+		}(i)
 
 	}
 	wait.Wait()
--- select.go
+++ select.go
@@ -3,19 +3,34 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
 
 func main() {
 	runtime.GOMAXPROCS(1)
-	int_chan := make(chan int, 1)
-	string_chan := make(chan string, 1)
-	int_chan <- 1
-	fmt.Println("test")
-	string_chan <- "hello"
+	intCh := make(chan int, 1)
+	stringCh := make(chan string, 1)
+	intCh <- 1
+	stringCh <- "hello"
 	select {
-	case value := <-int_chan:
+	case value := <-intCh:
 		fmt.Println(value)
-	case value := <-string_chan:
+	case value := <-stringCh:
 		panic(value)
+	}
+
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
 	}
 }

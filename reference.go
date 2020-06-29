8ccf8cdd8e0937e8261453015b7686b482a9cbcd
buggoing <tjbuggo@gmail.com>
2020-06-29 14:38:30 +0800

    add slice and map test

--- reference.go
+++ reference.go
@@ -10,7 +10,21 @@ func main() {
 	n := Node{
 		Val: []string{"hello", "world"},
 	}
+	fmt.Println("before: ", n)
 	n2 := n
 	n2.Val[0] = "world"
-	fmt.Println("n: ", n)
+	fmt.Println("after: ", n)
+
+	l1 := []string{"hello", "world"}
+	fmt.Println("before: ", l1)
+	l2 := l1
+	l2[0] = "world"
+	fmt.Println("after: ", l1)
+
+	m1 := make(map[string]string)
+	m1["hello"] = "hello"
+	fmt.Println("before: ", m1)
+	m2 := m1
+	m2["hello"] = "world"
+	fmt.Println("after: ", m1)
 }
